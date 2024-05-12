8e6c59601067f4098ac629bdc1e4941465355b27
svh <nwahs007@gmail.com>
2024-05-12 16:05:35 -0700

    "Hard" robot works, labels still don't update complete for turns.

--- app/src/main/java/com/example/tictactoe/PlayRobotActivity.java
+++ app/src/main/java/com/example/tictactoe/PlayRobotActivity.java
@@ -20,6 +20,7 @@ public class PlayRobotActivity extends AppCompatActivity implements View.OnClick
     private Button playerSwitch, diffSwitch;
     private List<TextView> winTexts = new ArrayList<>();
     private boolean isHardDiff = false;
+    private boolean hasTied = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -69,9 +70,10 @@ public class PlayRobotActivity extends AppCompatActivity implements View.OnClick
             ((Button)v).setText(PlayActivity.isXTurn ? "X": "O");
             PlayActivity.turnNum++;
             if(PlayActivity.turnNum >= 4 && winCheck(PlayActivity.checkDiagonalSum(index),
-                    PlayActivity.checkColumnSum(index), PlayActivity.checkRowSum(index)))
+                    PlayActivity.checkColumnSum(index), PlayActivity.checkRowSum(index))) {
                 nextGame();
-            else botTurn();
+            }
+            else botTurn(index);
         }
         else { //toast message indicating you cannot place there
             CharSequence text = "Spot's Taken";
@@ -80,54 +82,113 @@ public class PlayRobotActivity extends AppCompatActivity implements View.OnClick
     }
     private boolean winCheck(int a, int b, int c) //a, b, and c are the possible winning sums
     { //return boolean if game is finished it's true
+
+        //NOTE: change turn indicators when game finishes,
+        // should keep letters but swap player and bot turn precedence
+
+        //(on wincheck) if they tie the game should flip letter starts
+        //            if someone wins they take next first
+
         int win = Math.abs(a) == 3? a:
                 Math.abs(b) == 3 ? b:
                 Math.abs(c) == 3? c: -1;
         if(win != -1) {
             CharSequence text = win < 0 ? "O's Win!": "X's Win!";
-            Toast.makeText(this, text, Toast.LENGTH_LONG).show();
+            Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
             return true;
         }
         else if(PlayActivity.turnNum >= 9) {
             Toast.makeText(this, "Tie Game", Toast.LENGTH_LONG).show();
-            defaultTotal.setText((Integer.parseInt(defaultTotal.getText().toString()) -1) + "");
-            botTotal.setText((Integer.parseInt(botTotal.getText().toString()) -1) + "");
-            nextGame();
+            hasTied = true;
             return true;
         }
         else return false;
     }
     private void nextGame()
     {
-        if (PlayActivity.turnNum %2 != 0 == PlayActivity.isXTurn)
-            defaultTotal.setText((Integer.parseInt(defaultTotal.getText().toString()) + 1) + "");
-        else
-            botTotal.setText((Integer.parseInt(botTotal.getText().toString()) + 1) + "");
+        if(!hasTied) {
+            if (PlayActivity.turnNum %2 != 0 == PlayActivity.isXTurn)
+                defaultTotal.setText((Integer.parseInt(defaultTotal.getText().toString()) + 1) + "");
+            else
+                botTotal.setText((Integer.parseInt(botTotal.getText().toString()) + 1) + "");
+        }
+        hasTied = false;
         PlayActivity.grid = new int[9];
         PlayActivity.turnNum = 0;
         for (Button btn : BUTTONS)
             btn.setText("~");
     }
-    private void botTurn()
+    private void botTurn(int index)
     {
         PlayActivity.turnNum++;
-        int botIndex;
+        int botIndex = -1;
         if(isHardDiff)
         {
-            CharSequence text = "Hard Bot Turn";
-            Toast.makeText(this, text, Toast.LENGTH_LONG).show();
+            if(PlayActivity.turnNum > 2) {
+                if(PlayActivity.checkDiagonalSum(index) == 2) {
+                    if(index == 4) {
+                        int nonBlank = 0;
+                        //check corners
+                        while (PlayActivity.grid[nonBlank] == 0) nonBlank+= nonBlank == 2 ? 4: 2;
+                        botIndex= 8 -nonBlank;
+                    }
+                    else if(PlayActivity.grid[4] == 0)
+                    {
+                        botIndex = 4;
+                    }
+                    else {
+                        int endD = (index % 4 == 0) ? 9 : 7;
+                        int addD = (endD == 9) ? 4 : 2;
+                        for (int i = index % 4; i < endD; endD += addD) {
+                            if (PlayActivity.grid[i] == 0) {
+                                botIndex = i;
+                                break;
+                            }
+                        }
+                    }
+                    //this leaves a hole in the hard diff bot,
+                    //only if you take the center and play a diagonal
+                } //end of partical digaonal case
+                else if(PlayActivity.checkRowSum(index) == 2) {
+                    int startR = index<3? 0: index<6?3:6;
+                    for(int j = startR; j< startR+3; j++) {
+                        if (PlayActivity.grid[j] == 0) {
+                            botIndex = j;
+                            break;
+                        }
+                    }
+                }
+                else if(PlayActivity.checkColumnSum(index) == 2) {
+                    int startC = index%3;
+                    for(int k = startC; k< startC+7; k+=3) {
+                        if (PlayActivity.grid[k] == 0) {
+                            botIndex = k;
+                            break;
+                        }
+                    }
+                }
+            } //end of turn num if
+        } //end of hard diff
+        if(botIndex == -1) botIndex = nextRandom();
+        //NOTE: make sure the botIndex you set is not one that already exists,
+        // otherwise you will override existing tile
 
-        }
-        else {
-            //add the easy diff code in here I guess
-            // starter ideas, Math.random to get an index and make sure it's not a spot already taken in grid[]
+        PlayActivity.grid[botIndex] = PlayActivity.isXTurn ? -1: 1;
+        BUTTONS.get(botIndex).setText(PlayActivity.isXTurn? "O": "X");
 
-        }
 //        uncomment these lines after adding both difficulties
 //        adds the winCheck after botTurn
-/*        if(PlayActivity.turnNum >= 4 && winCheck(PlayActivity.checkDiagonalSum(botIndex),
-                PlayActivity.checkColumnSum(botIndex), PlayActivity.checkRowSum(botIndex)))
-            nextGame();*/
+        if(PlayActivity.turnNum >= 4 && winCheck(PlayActivity.checkDiagonalSum(botIndex),
+                PlayActivity.checkColumnSum(botIndex), PlayActivity.checkRowSum(botIndex))) {
+            nextGame();
+        }
+    }
+    private void botStart()
+    {
+        int addIndex = (Math.random()*9 <= 4)? (int)(Math.random() * 8): 4;
+        PlayActivity.grid[addIndex] = PlayActivity.isXTurn? -1: 1;
+        BUTTONS.get(addIndex).setText(PlayActivity.isXTurn? "O": "X");
+        PlayActivity.turnNum++;
     }
     public void openHome(View v)
     {
@@ -142,25 +203,27 @@ public class PlayRobotActivity extends AppCompatActivity implements View.OnClick
             playerLetter.setText("Player " + (PlayActivity.isXTurn ? "X's START": "O's"));
             if(!PlayActivity.isXTurn) {
                 botLetter.setText("Bot X's START");
-                int addIndex = (Math.random()*9 <= 4)? (int)(Math.random() * 8): 4;
-                PlayActivity.grid[addIndex] = 1;
-                BUTTONS.get(addIndex).setText("X");
-                PlayActivity.turnNum++;
+                botStart();
             }
             else {
                 botLetter.setText("Bot O's");
             }
         }
         else {
-            CharSequence text = "Finish Game First.";
-            Toast.makeText(this, text, Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Finish Current Game First.", Toast.LENGTH_SHORT).show();
         }
     }
     public void diffChangeClick(View v)
     {
         isHardDiff^=true;
-        if(isHardDiff) Toast.makeText(this, "Hard Mode Enabled", Toast.LENGTH_SHORT).show();
-        else Toast.makeText(this, "Hard Mode Disabled", Toast.LENGTH_SHORT).show();
+        CharSequence text = isHardDiff ? "Hard Mode Enabled": "Hard Mode Disabled";
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
         //optionally add some other indicator that game is in hard mode eg. UI changes color
+    }
+    public int nextRandom()
+    {
+        int i = (int)(Math.random()*8);
+        while(PlayActivity.grid[i]!= 0) i = (int)(Math.random()*8);
+        return i;
     }
 }
